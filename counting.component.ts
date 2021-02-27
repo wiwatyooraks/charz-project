e51f1af071089629f65de236530183921a0157a2
wiwatyooraks <78940910+wiwatyooraks@users.noreply.github.com>
2021-02-27 09:50:02 +0700

    Learn Angular
    10 11 13

---
+++ counting.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+selector: 'app-counting',
+templateUrl: './counting.component.html',
+styleUrls: ['./counting.component.css']
+
+})
+export class CountingComponent {
+    @Input()
+    title:string;
+
+    count = 0;
+    constructor() {
+    } 
+    countNumber() {
+        this.count = this.count + 1;
+    }
+
+}
